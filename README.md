259e0d874a5be7df52fb5d94f371343629fdbd2d
hazcod <hello@ironpeak.be>
2021-02-04 16:04:20 +0100

    feat: add schedule/prune

--- README.md
+++ README.md
@@ -1,25 +1,59 @@
 
 # macbac
 
-Lists and controls your macOS snapshots and backups for all your available modules.
-This is a pretty wrapper around tmutil for your convenience.
+Lists, controls and schedules efficient APFS snapshots for your convenience.
 
 ```shell
-% macbac status
-Status: Inactive
+# let's take space efficient APFS snapshots every hour
+# knowing they get cleaned up if necessary
+% ./macbac.sh schedule hourly
+Removing previous schedule...
+Removed previous snapshot schedule.
+Installing daemon config to /Users/user/Library/LaunchAgents/com.hazcod.macbac.plist
+Loading config to enable schedule...
+Scheduled hourly snapshots!
 
+# show our current snapshots
 % macbac list
 /Volumes/SSD
 > 2020/12/30 10:23
 > 2020/12/30 11:04
 > 2020/12/30 11:05
 
+# take a manual snapshot
 % macbac snapshot
 Assuming / is the volume we would like to snapshot.
 Created local snapshot with date: 2020-12-30-111728
 Snapshotted volume /
+
+# let's remove snapshots but ensure we keep the 3 most recent
+% macbac prune 3
+Pruning 1 of 4 snapshots for /
+Pruning snapshot 2021-02-04-155845 (1/1)
 ```
+
+## How does it work?
+
+It's a convenient wrapper around `tmutil`.
 
 ## Installation
 
-Just copy `macbac.sh` to  your filesystem.
+Installation can be done straight from [my Homebrew tap](https://github.com/hazcod/homebrew-hazcod) via `brew install hazcod/homebrew-hazcod/macbac` or just copy `macbac.sh` to  your filesystem.
+
+## Usage
+
+`Usage: macbac <status|list|snapshot|enable|disable|schedule|deschedule|prune> <...>`
+
+To view Time Machine status: `macbac status`
+
+To take a snapshot: `macbac snapshot`
+
+To take a snapshot and keep 3: `macbac snapshot 3`
+
+To prune but keep 5: `macbac prune 5`
+
+To schedule hourly snapshots, keeping 24: `macbac schedule hourly`
+
+To schedule hourly snapshots, keeping 3: `macbac schedule hourly 3` 
+
+To schedule daily snapshots, keeping 7: `macbac schedule daily`
--- macbac.sh
+++ macbac.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+PACKAGE_NAME="com.hazcod.macbac"
+PLIST_PATH="$HOME/Library/LaunchAgents/${PACKAGE_NAME}.plist"
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
@@ -12,30 +15,166 @@ BOLD='\033[1m'
 
 usage() {
     # show command cli usage help
-    echo "Usage: $0 <status|list|snapshot|enable|disable>"
+    echo "Usage: $0 <status|list|snapshot|enable|disable|schedule|deschedule|prune> <...>"
     exit 1
 }
 
 error() {
     # show an error message and exit
-    echo -e "${RED}ERROR:${N} ${1}${NC}"
+    >&2 echo -e "${RED}ERROR:${N} ${1}${NC}"
     exit 1
 }
 
+realpath() {
+  OURPWD=$PWD
+  cd "$(dirname "$1")"
+  LINK=$(readlink "$(basename "$1")")
+  while [ "$LINK" ]; do
+    cd "$(dirname "$LINK")"
+    LINK=$(readlink "$(basename "$1")")
+  done
+  REALPATH="$PWD/$(basename "$1")"
+  cd "$OURPWD"
+  echo "$REALPATH"
+}
+
+deschedule() {
+    if [ ! -f "${PLIST_PATH}" ]; then
+        error "a schedule was not configured"
+        exit 1
+    fi
+
+    launchctl stop "${PACKAGE_NAME}" 2>/dev/null
+
+    if ! launchctl unload "$PLIST_PATH" || ! rm "${PLIST_PATH}"; then
+        error "could not disable the schedule"
+        exit 1
+    fi
+
+    echo -e "Removed previous snapshot schedule."
+}
+
+schedule() {
+    local mode="$1"
+    local keep="$2"
+
+    interval=""
+    if [ "$mode" == "hourly" ]; then
+        interval="<key>Minute</key><integer>$(($(date +%M) +1))</integer>"
+        if [ -z "$keep" ]; then
+            keep="24"
+        fi
+    elif [ "$mode" == "daily" ]; then
+        interval="<key>Hour</key><integer>$(($(date +%H) -1))</integer>"
+        if [ -z "$keep" ]; then
+            keep="7"
+        fi
+    else
+        error "invalid schedule mode: ${mode}"
+        exit 1
+    fi
+
+    if [ -f "$PLIST_PATH" ]; then
+        echo "Removing previous schedule..."
+        deschedule
+    fi
+
+    echo "Installing daemon config to ${PLIST_PATH}"
+    cat >"$PLIST_PATH" <<EOL
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>${PACKAGE_NAME}</string>
+    <key>Nice</key>
+    <integer>20</integer>
+    <key>StandardOutPath</key>
+    <string>/tmp/macbac.log</string>
+    <key>StandardErrorPath</key>
+    <string>/tmp/macbac.err</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>$(realpath $0)</string>
+        <string>snapshot</string>
+        <string>${keep}</string>
+    </array>
+    <key>StartCalendarInterval</key>
+    <dict>
+        ${interval}
+    </dict>
+</dict>
+</plist>
+EOL
+
+    echo "Loading config to enable schedule..."
+    if ! launchctl load "$PLIST_PATH" || ! launchctl start "${PACKAGE_NAME}"; then
+        error "Could not enable the schedule"
+        exit 1
+    fi
+
+    echo -e "${GREEN}Scheduled ${mode} snapshots!${NC}"
+}
+
+prune() {
+    local volume="$1"
+    local amount="$2"
+
+    if [[ ${amount} -le 0 ]]; then
+        exit 0
+    fi
+
+    IFS=$'\n' read -r -d '' -a snapshots < <(tmutil listlocalsnapshotdates "$volume" | tail -n +2 | sort)
+
+    if [[ ${#snapshots[@]} -eq 0 ]]; then
+        echo "No snapshots to purge"
+        exit 0
+    fi
+
+    if [[ ${amount} -gt ${#snapshots[@]} ]]; then
+        amount=0
+    else
+        amount=$((${#snapshots[@]} - amount))
+    fi
+
+    echo "Pruning ${amount} of ${#snapshots[@]} snapshots for ${volume}"
+    
+    counter="$amount"
+    for snapshot in "${snapshots[@]}"; do
+        if [[ $counter -le 0 ]]; then
+            break
+        fi
+
+        echo "Pruning snapshot ${snapshot} (${counter}/${amount})"
+
+        if ! tmutil deletelocalsnapshots "${snapshot}" >/dev/null; then
+            error "could not prune local snapshot ${snapshot}"
+            exit 1
+        fi
+
+        counter=$((counter -1))
+    done
+}
+
 snapshot() {
-    local volume="$2"
+    local volume="$1"
+    local pruneAmount="$2"
 
     if [ -z "$volume" ]; then
         echo "Assuming / is the volume we would like to snapshot."
         volume="/"
     fi
 
-    if ! tmutil localsnapshot "$volume" | grep -v '^NOTE:'; then
+    if ! tmutil localsnapshot "${volume}" | grep -v '^NOTE:'; then
         error "Could not snapshot ${volume}"
         exit 1
     fi
 
     echo -e "${GREEN}Snapshotted volume ${volume}${NC}"
+
+    if [ -n "$pruneAmount" ] && (( pruneAmount > 0 )); then
+        prune "$volume" "$pruneAmount"
+    fi
 }
 
 enable() {
@@ -70,16 +209,16 @@ listStatus() {
     status="$(tmutil currentphase)"
     
     if [ "$status" == "BackupNotRunning" ]; then
-        echo -e "${BOLD}Status${NC}: Inactive"
+        echo -e "${BOLD}Backup Status${NC}: Inactive"
         return
     fi
 
     if [ "$status" == "BackupError" ]; then
-        echo -e "${BOLD}Status${NC}: ${RED}Error during backup${NC}"
+        echo -e "${BOLD}Backup Status${NC}: ${RED}Error during backup${NC}"
         return
     fi
 
-    echo -e "${BOLD}Status${NC}: ${GREEN}Running${NC}"
+    echo -e "${BOLD}Backup Status${NC}: ${GREEN}Running${NC}"
 }
 
 listSnapshots() {
@@ -100,7 +239,7 @@ listSnapshots() {
         # list all snapshots
         for snapshot in $snapshots; do
             IFS='-' read -ra parts <<< "$snapshot"
-            dateStr="${parts[3]:0:2}:${parts[3]:2:2}"
+            dateStr="${parts[3]:0:2}:${parts[3]:2:2}:${parts[3]:4:2}"
             echo "> ${parts[0]}/${parts[1]}/${parts[2]} $dateStr"
         done
 
@@ -115,13 +254,13 @@ listSnapshots() {
 #--------------------------------------------------------------------------------------------------------------------------------------
 #
 
-if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+if [ $# -lt 1 ]; then
     usage
 fi
 
 case "$1" in
     "status")
-        if [ $# -ne 1 ]; then
+        if [[ "$#" -gt 1 ]]; then
             usage
         fi
         
@@ -129,7 +268,7 @@ case "$1" in
     ;;
 
     "list")
-        if [ $# -ne 1 ]; then
+        if [[ "$#" -gt 1 ]]; then
             usage
         fi
         
@@ -137,33 +276,51 @@ case "$1" in
     ;;
 
     "snapshot")
-        if [ $# -ne 1 ]; then
-            if [ $# -ne 2 ]; then
-                usage
-            fi
+        if [[ "$#" -gt 2 ]]; then
+            usage
         fi
         
-        snapshot "$1"
+        snapshot "/" "$2"
     ;;
 
     "enable")
-        if [ $# -ne 1 ]; then
-            if [ $# -ne 2 ]; then
-                usage
-            fi
+        if [[ "$#" -gt 1 ]]; then
+            usage
         fi
         
-        enable "$1"
+        enable
     ;;
 
     "disable")
-        if [ $# -ne 1 ]; then
-            if [ $# -ne 2 ]; then
-                usage
-            fi
+        if [[ "$#" -gt 1 ]]; then
+            usage
         fi
         
-        disable "$1"
+        disable
+    ;;
+
+    "schedule")
+        if [[ "$#" -gt 3 ]]; then
+            usage
+        fi
+        
+        schedule "$2" "$3"
+    ;;
+
+    "deschedule")
+        if [[ "$#" -gt 1 ]]; then
+            usage
+        fi
+        
+        deschedule
+    ;;
+
+    "prune")
+        if [[ "$#" -gt 2 ]]; then
+            usage
+        fi
+        
+        prune "/" "$2"
     ;;
 
     *)
